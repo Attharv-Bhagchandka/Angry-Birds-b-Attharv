a69b06e0e61b543884046408a2f4622cc4bf0716
Attharv-Bhagchandka <76523111+Attharv-Bhagchandka@users.noreply.github.com>
2021-05-11 19:22:14 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,19 +1,17 @@
-class Ground{
-    constructor(x,y,w,h){
-        var options={
-            isStatic: true
-        }
-        this.body = Bodies.rectangle(x,y,w,h, options);
-        this.width = w;
-        this.height = h;
-
-        World.add(world, this.body);
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
     }
     display(){
-        fill("brown");
-        var Pos = this.body.position;
-
-        rectMode(CENTER);
-        rect(Pos.x, Pos.y, this.width, this.height);
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
     }
-}
+  };
--- sketch.js
+++ sketch.js
@@ -1,62 +1,83 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
-var Box1, Box2, Box3, Box4, Box5;
-var bird, Platform, Level; 
-var Pig1, Pig2;
-var Log1, Log2, Log3, Log4;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
 
-function setup() {
-  createCanvas(1200,400);
-
-  engine = Engine.create();
-  world = engine.world;
-  
-  Level = new Ground(600, 390, 1200, 20);
-  Platform = new Ground(150, 305, 300, 170)
-  bird = new Bird(100, 100);
-
-  Box1 = new Box(700, 320, 70, 70);
-  Box2 = new Box(920, 320, 70, 70);
-  Pig1 = new Pig(810, 350);
-  Log1 = new Log(810, 260, 300, PI/2);
-
-  Box3 = new Box(700, 240, 70, 70);
-  Box4 = new Box(920, 240, 70, 70);
-  Pig2 = new Pig(810, 220);
-  Log2 = new Log(810, 180, 300, PI/2);
-  
-  Box5 = new Box(810, 160, 70, 70);
-  Log3 = new Log(760, 120, 150, PI/7);
-  Log4 = new Log(870, 120, 150, -PI/7);
-
-  
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
-function draw() {
-  background('black');  
-  Engine.update(engine)
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
-  Box1.display();
-  Box2.display();
-  Pig1.display();
-  Log1.display();
 
-  Box3.display();
-  Box4.display();
-  Pig2.display();
-  Log2.display();
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
-  Log3.display();
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
 
-  Box5.display();
-  Log3.display();
-  Log4.display();
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
 
-  bird.display();
-  Level.display();
-  Platform.display();
-  
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode == 32){
+      slingShot.attatch(bird.body);
+    }
+  }
